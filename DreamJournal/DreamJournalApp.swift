974f2a2271e49e2fd0f179223a9d1c76b4061f21
Chris <Rowley>
2023-08-04 00:23:44 -0700

    Initial Commit

---
+++ DreamJournal/DreamJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamJournalApp.swift
+//  DreamJournal
+//
+//  Created by Chris Rowley on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DreamJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
